08bbcff155caedcb7ab1ac2b45544fbdefbca871
juniortemgoua0 <juniortemgoua0@gmail.com>
2021-12-15 02:51:35 +0100

    Ajout de l'effet de decompte a la section statistic lors du defilement

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let btnFigure = document.querySelector(".btn_figure")
 let iconFleche = document.querySelector(".arrow_right")
 let categoryCount = document.querySelector(".categorie_count")
+
 btnFigure.addEventListener("mouseenter", (e) => {
     iconFleche.style.marginLeft = "30px"
     iconFleche.style.fontSize = "27px"
@@ -24,6 +25,33 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 entry.target.classList.add("reveal_y_visible")
                 observer.unobserve(entry.target)
             }
+            if (entry.target.classList.contains("categorie_count")) {
+                console.log("Count observer")
+                let value = entry.target.textContent
+                let i = 0
+                if (value.includes("K")) {
+                    valueConvert = parseInt(value.slice(0, value.length - 1))
+                    console.log(valueConvert)
+                    let interval = window.setInterval(function() {
+
+                        entry.target.innerText = i + "K"
+                        i++
+                        if (entry.target.textContent == valueConvert + "K") {
+                            clearInterval(interval)
+                        }
+                    }, 100)
+
+                } else {
+                    let interval = window.setInterval(function() {
+                        entry.target.innerText = i
+                        i++
+                        if (entry.target.textContent == value) {
+                            clearInterval(interval)
+                        }
+                    }, 50)
+                }
+
+            }
 
         })
     }
@@ -36,6 +64,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
         observer.observe(r)
     })
     document.querySelectorAll(".reveal_x_rigth").forEach(function(r) {
+        observer.observe(r)
+    })
+    document.querySelectorAll(".categorie_count").forEach(function(r) {
         observer.observe(r)
     })
 });
